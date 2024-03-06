c2e678efc36224358902ad0e347d75c3db78d5c7
dribble-njr <wzw15292257101@163.com>
2024-03-06 13:33:59 +0800

    feat: index page

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import LayOut from '../components/layouts/main'
+
+export default function App({ Component, pageProps, router }: AppProps) {
+  return (
+    <ChakraProvider>
+      <LayOut router={router}>
+        <Component {...pageProps} key={router.route} />
+      </LayOut>
+    </ChakraProvider>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,55 @@
+import { Box, Container, Heading } from '@chakra-ui/react'
+import Image from 'next/image'
+import { Logo } from '../components'
+
 export default function Page() {
-  return <div>hello</div>
+  return (
+    <Container>
+      <Box
+        borderRadius="lg"
+        mb={6}
+        p={3}
+        textAlign="center"
+        css={{ backdropFilter: 'blur(10px)' }}
+      >
+        Hello, I&apos;m an frontend developer!
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            njr
+          </Heading>
+          <p>Enquanto houver 1% de chance, teremo 99% fé.</p>
+          <Logo />
+        </Box>
+
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+          <Box
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            w="100px"
+            h="100px"
+            display="inline-block"
+            borderRadius="full"
+            overflow="hidden"
+          >
+            <Image
+              src="/images/avatar.jpeg"
+              alt="Profile image"
+              width="100"
+              height="100"
+            />
+          </Box>
+        </Box>
+      </Box>
+      <div>hello</div>
+    </Container>
+  )
 }
