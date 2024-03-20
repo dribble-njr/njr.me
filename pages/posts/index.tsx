12bfb12520ef4dad7c53f3eb9d81894a45806352
dribble-njr <wzw15292257101@163.com>
2024-03-20 23:14:30 +0800

    feat: handle front matter

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,11 +1,13 @@
-import { readdir } from 'fs/promises'
+import { readFile, readdir } from 'fs/promises'
+import matter from 'gray-matter'
 import { join } from 'path'
+import { Post } from '../../@interface/post'
 
-export default function Posts({ posts }) {
+export default function Posts({ posts }: { posts: Post[] }) {
   return (
     <>
-      {posts.map(slug => (
-        <p key={slug}>{slug}</p>
+      {posts.map(post => (
+        <p key={post.slug}>{post.title}{post.date}{post.summary}</p>
       ))}
     </>
   )
@@ -15,7 +17,24 @@ const getPosts = async () => {
   const postsDirectory = join(process.cwd(), 'posts')
   const entries = await readdir(postsDirectory)
 
-  return entries
+  const fileContents = await Promise.all(
+    entries.map(entry => readFile(postsDirectory + `/${entry}`, 'utf8'))
+  )
+
+  const posts = entries.map((entry, i) => {
+    const fileContent = fileContents[i]
+    const { data } = matter(fileContent)
+    const date = new Date(data.date).toISOString()
+    return { slug: entry, ...data, date } as Post
+  })
+
+  posts.sort((a, b) => {
+    return Date.parse(b.date) - Date.parse(a.date)
+  })
+
+  console.log(fileContents, '===', posts)
+
+  return posts
 }
 
 export async function getStaticProps() {
--- posts/how-to-build-this-blog.md
+++ posts/how-to-build-this-blog.md
@@ -1,1 +1,7 @@
+---
+title: how to build this blog
+date: 2024-03-20
+summary: step by step, build the blog with next.js, chakra ui, three.js and etc.
+---
+
 ## 技术栈
