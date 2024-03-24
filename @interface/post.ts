b26b4872c3c595255cb4440d2b6853656c54ad6f
dribble-njr <wzw15292257101@163.com>
2024-03-24 23:14:57 +0800

    feat(interface): metadata

--- @interface/post.ts
+++ @interface/post.ts
@@ -1,6 +1,8 @@
-export interface Post {
-  slug: string
+export interface MetaData {
   title: string
   date: string
   summary: string
+}
+export interface Post extends MetaData {
+  slug: string
 }
