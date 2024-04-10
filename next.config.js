4d57ea25e432ef418da09da8c624a91b9a8dca47
dribble-njr <wzw15292257101@163.com>
2024-04-10 12:48:20 +0800

    chore: set basePath

--- next.config.js
+++ next.config.js
@@ -1,8 +1,19 @@
+const isGitHubActions = process.env.GITHUB_ACTIONS || false
+
+let basePath = '/'
+
+if (isGitHubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  basePath = `/${repo}`
+}
+
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'export',
   images: {
     unoptimized: true
-  }
+  },
+  basePath
 }
