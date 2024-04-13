640a39050d573cf43937102f6831748eea3306f1
dribble-njr <wzw15292257101@163.com>
2024-04-13 12:35:49 +0800

    Deploy by vercel

--- next.config.js
+++ next.config.js
@@ -1,23 +1,5 @@
-const isGitHubActions = process.env.GITHUB_ACTIONS || false
-
-let basePath = ''
-let assetPrefix = ''
-
-if (isGitHubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  basePath = `/${repo}`
-  assetPrefix = `/${repo}/`
-}
-
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'export',
-  images: {
-    unoptimized: true
-  },
-  basePath,
-  assetPrefix
 }
